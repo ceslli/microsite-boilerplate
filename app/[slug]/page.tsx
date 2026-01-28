8bbf0ce0034570ee68685f3c77105f1fa5da4356
Cesim Civisilli <ces.civisil@gmail.com>
2026-01-28 17:54:28 +0200

    use search params to display individual page data from contentful

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,52 +1,8 @@
-import { getAllPages, getPage } from "@/lib/contentful";
-import Link from "next/link";
+import { getPage } from "@/lib/contentful";
 
-export default async function Slug() {
-  const pages = await getAllPages();
-  console.log(pages);
-  const page = await getPage(pages[0].sys.id);
-  console.log(page);
-  return (
-    pages && (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-        <section className="w-full pt-12">
-          <div className="mx-auto container space-y-12 px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  Welcome to our Knowledge Base
-                </h1>
-              </div>
-            </div>
-          </div>
-          <div className="space-y-12">
-            <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-              {pages.map((article) => (
-                <article
-                  key={article.sys.id}
-                  className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden"
-                >
-                  <div className="flex-1 p-6">
-                    <Link href={`/articles/${article.slug}`}>
-                      <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
-                        {article.title}
-                      </h3>
-                    </Link>
-                  </div>
-                  <div className="flex justify-end">
-                    <Link
-                      className="inline-flex h-10 items-center justify-center text-sm font-medium"
-                      href={`/articles/${article.slug}`}
-                    >
-                      Read More →
-                    </Link>
-                  </div>
-                </article>
-              ))}
-            </div>
-          </div>
-        </section>
-      </main>
-    )
-  );
+export default async function Slug(props: PageProps<"/[slug]">) {
+  const query = await props.searchParams;
+  const page = await getPage(query.id);
+  console.log("hello", page);
+  return <div>{page.title}</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,55 @@
-export default function Home() {
-  return <></>;
+import { getAllPages, getPage } from "@/lib/contentful";
+import Link from "next/link";
+
+export default async function Home() {
+  const pages = await getAllPages();
+  console.log(pages);
+  return (
+    pages && (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+        <section className="w-full pt-12">
+          <div className="mx-auto container space-y-12 px-4 md:px-6">
+            <div className="flex flex-col items-center justify-center space-y-4 text-center">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  Welcome to our Knowledge Base
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="space-y-12">
+            <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+              {pages.map((page) => (
+                <article
+                  key={page.sys.id}
+                  className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden"
+                >
+                  <div className="flex-1 p-6">
+                    <Link href={`/articles/${page.slug}`}>
+                      <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
+                        {page.title}
+                      </h3>
+                    </Link>
+                  </div>
+                  <div className="flex justify-end">
+                    <Link
+                      className="inline-flex h-10 items-center justify-center text-sm font-medium"
+                      href={{
+                        pathname: `/${page.slug}`,
+                        query: {
+                          id: page.sys.id,
+                        },
+                      }}
+                    >
+                      Read More →
+                    </Link>
+                  </div>
+                </article>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+    )
+  );
 }
